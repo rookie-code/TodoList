53047fdbc2baf2a4bb34f8321beb66c7f3c59302
rookie-code <mscsrl@hotmail.it>
2021-04-12 00:19:07 +0200

    update project

--- obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
@@ -13,70 +13,70 @@ namespace TodoList.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
+#line 1 "c:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
+#line 2 "c:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
+#line 3 "c:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
+#line 4 "c:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
+#line 5 "c:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
+#line 6 "c:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
+#line 7 "c:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
+#line 8 "c:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
+#line 9 "c:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
 using TodoList;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
+#line 10 "c:\Users\LineaMsc\Desktop\blazor\TodoList\_Imports.razor"
 using TodoList.Shared;
 
 #line default
@@ -91,7 +91,7 @@ using TodoList.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 9 "C:\Users\LineaMsc\Desktop\blazor\TodoList\Pages\Counter.razor"
+#line 9 "c:\Users\LineaMsc\Desktop\blazor\TodoList\Pages\Counter.razor"
        
     private int currentCount = 0;
 
